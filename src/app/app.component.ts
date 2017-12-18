77f0ab26465f33ae6fdf65343d2cb37112c5b48a
Hugo Cicarelli <hcicarelli@gmail.com>
2017-12-18 10:24:33 -0200

    Validações Formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
     nextStep = true;
     btnFinish = false;
-    $ = jQuery;
     planID = 0;
 
     //variáveis das opções de planos de assinatura
@@ -26,12 +25,13 @@ export class AppComponent {
 
     creditoTexto = '';
     debitoTexto = '';
-    boletoTexto = ''
+    boletoTexto = '';
+    planDetails = '';
 
     constructor() {
         var dataJson;
 
-        this.$.ajax({
+        jQuery.ajax({
             dataType: 'json',
             method: 'GET',
             url: '../assets/js/data.json',
@@ -44,24 +44,25 @@ export class AppComponent {
             this.firstName = dataJson[0].name;
             this.secondName = dataJson[1].name;
             this.thirdName = dataJson[2].name;
-            this.$('#plano-1 .economize').html(dataJson[0].discount);
-            this.$('#plano-2 .economize').html(dataJson[1].discount);
-            this.$('#plano-3 .economize').html(dataJson[2].discount);
+            jQuery('#plano-1 .economize').html(dataJson[0].discount);
+            jQuery('#plano-2 .economize').html(dataJson[1].discount);
+            jQuery('#plano-3 .economize').html(dataJson[2].discount);
 
             this.valorCurrency = dataJson[this.planID].price.currency;
             this.valorInteger = dataJson[this.planID].price.integer;
             this.valorDecimal = dataJson[this.planID].price.decimal;
             this.valorPeriodo = dataJson[this.planID].price.periodicy;
+            this.planDetails = dataJson[this.planID].details;
 
-            this.$('.valor-total-container p').html(dataJson[this.planID].total);
-            this.$('.pagamento-tipo.credito p').html(dataJson[this.planID].payments[0]);
-            this.$('.pagamento-tipo.debito p').html(dataJson[this.planID].payments[1]);
-            this.$('.pagamento-tipo.boleto p').html(dataJson[this.planID].payments[2]);
+            jQuery('.valor-total-container p').html(dataJson[this.planID].total);
+            jQuery('.pagamento-tipo.credito p').html(dataJson[this.planID].payments[0]);
+            jQuery('.pagamento-tipo.debito p').html(dataJson[this.planID].payments[1]);
+            jQuery('.pagamento-tipo.boleto p').html(dataJson[this.planID].payments[2]);
         }, 5);
     }
 
     choosePlanOption(event: any) {
-        this.$('.opcao-box').removeClass('active');
+        jQuery('.opcao-box').removeClass('active');
 
         if ( (<HTMLInputElement>event.target).className == 'opcao-box' ) {
             (<HTMLInputElement>event.target).className += " active";
@@ -72,9 +73,9 @@ export class AppComponent {
         }
 
         var dataJson;
-        this.planID = this.$('.opcao-box.active').attr('data-id');
+        this.planID = jQuery('.opcao-box.active').attr('data-id');
 
-        this.$.ajax({
+        jQuery.ajax({
             dataType: 'json',
             method: 'GET',
             url: '../assets/js/data.json',
@@ -88,11 +89,12 @@ export class AppComponent {
             this.valorInteger = dataJson[this.planID].price.integer;
             this.valorDecimal = dataJson[this.planID].price.decimal;
             this.valorPeriodo = dataJson[this.planID].price.periodicy;
+            this.planDetails = dataJson[this.planID].details;
 
-            this.$('.valor-total-container p').html(dataJson[this.planID].total);
-            this.$('.pagamento-tipo.credito p').html(dataJson[this.planID].payments[0]);
-            this.$('.pagamento-tipo.debito p').html(dataJson[this.planID].payments[1]);
-            this.$('.pagamento-tipo.boleto p').html(dataJson[this.planID].payments[2]);
+            jQuery('.valor-total-container p').html(dataJson[this.planID].total);
+            jQuery('.pagamento-tipo.credito p').html(dataJson[this.planID].payments[0]);
+            jQuery('.pagamento-tipo.debito p').html(dataJson[this.planID].payments[1]);
+            jQuery('.pagamento-tipo.boleto p').html(dataJson[this.planID].payments[2]);
         }, 5);
     }
 
@@ -100,20 +102,24 @@ export class AppComponent {
         // caso seja para avançar para o formulário do cartão
         if ( this.nextStep && !this.btnFinish ) {
             // mainBtn.remove("next").className += " finish";
-            this.$('#payment-info').slideUp().next().slideDown();
-            this.$('#steps-btn').text('Concluir minha assinatura');
-            this.$('.plano-flow .flow-content:first-of-type').removeClass('active').next().addClass('active');
+            jQuery('#payment-info').slideUp().next().slideDown();
+            jQuery('#steps-btn').text('Concluir minha assinatura');
+            jQuery('.plano-flow .flow-content:first-of-type').removeClass('active').next().addClass('active');
 
             this.nextStep = false;
             this.btnFinish = true;
         } else {
-            this.$('#payment-data, .plano-flow, .vipmodal-footer').slideUp();
-            this.$('#payment-end').slideDown();
+            jQuery('#payment-data, .plano-flow, .vipmodal-footer').slideUp();
+            jQuery('#payment-end').slideDown();
         }
     }
 
-    validateForm(event: any) {
-        console.log(<HTMLInputElement>event.target);
+    validateForm() {
+        var date = new Date();
+        var monthNumber = date.getMonth() + 1;
+        var year = date.getFullYear();
+
+        // console.log((<HTMLInputElement>event.target).value);
         // if ( (<HTMLInputElement>event.target).className == 'opcao-box' ) {
         //     (<HTMLInputElement>event.target).className += " active";
         // } else if ( (<HTMLInputElement>event.target).className == 'percetage' ) {
